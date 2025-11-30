5169aa3433b48ed6e3aeda2dfd15a21118305a98
Kelly Maass <kmaass77627@gmail.com>
2025-11-30 11:11:16 -0600

    new

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employee) {
+    let newEmployee = {
+        firstName: employee.firstName,
+        familyName: employee.familyName,
+        title: employee.title,
+        payPerHour: employee.payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+        }
+   return newEmployee
+}
+
+function createEmployeeRecords(employees) {
+
+ let newEmployees = employees.map((employee) => createEmployeeRecord(employee))
+ return newEmployees
+}
+
+function createTimeInEvent(dateTime) {
+    let hour = dateTime.split(" ")[1].slice(0,2)
+    console.log(hour)
+    let date = dateTime.split(" ")[0]
+    console.log(date)
+    let timeIn = {
+        type: "TimeIn",
+        hour: hour,
+        date: date,
+    }
+    return timeIn
+}
+
+function createTimeOutEvent() {
+
+}
+
+function hoursWorkedOnDate() {
+
+}
+
+function wagesEarnedOnDate() {
+
+}
+
+// function allWagesFor() {
+
+// }
+
+function findEmployeeByFirstName() {
+
+}
+
+function calculatePayroll() {
+
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +75,8 @@ const allWagesFor = function () {
 
     return payable
 }
+
+
+
+
 
